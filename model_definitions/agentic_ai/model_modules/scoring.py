ac361ea6bf07700c419d6d5d1f3e02c2d767f260
Vasu2803k <katravathvasu000@gmail.com>
2025-08-28 11:20:21 +0000

    Added Microsoft autogen files

--- model_definitions/agentic_ai/model_modules/scoring.py
+++ model_definitions/agentic_ai/model_modules/scoring.py
@@ -1,2 +1,8 @@
+from aoa import (
+    record_scoring_stats,
+    aoa_create_context,
+    ModelContext
+)
 def score(context: ModelContext, **kwargs):
+    aoa_create_context()
     print("Scoring")
