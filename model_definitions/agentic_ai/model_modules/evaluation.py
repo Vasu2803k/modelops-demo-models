39f274188c74341ea14030f37e14921df99496e7
Vasu2803k <katravathvasu000@gmail.com>
2025-08-28 11:18:58 +0000

    Added Microsoft autogen files

--- model_definitions/agentic_ai/model_modules/evaluation.py
+++ model_definitions/agentic_ai/model_modules/evaluation.py
@@ -2,11 +2,12 @@ import json
 from tmo import ModelContext
 
 def evaluate(context: ModelContext, **kwargs):
-    config = {
-        "LLM_MODEL": context.hyperparams["LLM_MODEL"],
-        "LLM_BASE_URL": context.hyperparams["LLM_BASE_URL"],
-        "LLM_API_KEY": context.hyperparams["LLM_API_KEY"],
-    }
+    print("Evaluating")
+#     config = {
+#         "LLM_MODEL": context.hyperparams["LLM_MODEL"],
+#         "LLM_BASE_URL": context.hyperparams["LLM_BASE_URL"],
+#         "LLM_API_KEY": context.hyperparams["LLM_API_KEY"],
+#     }
 
-    with open(f"{context.artifact_output_path}/model_config.json", "w") as f:
-        json.dump(config, f)
+#     with open(f"{context.artifact_output_path}/model_config.json", "w") as f:
+#         json.dump(config, f)
