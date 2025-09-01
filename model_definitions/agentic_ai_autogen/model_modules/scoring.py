5567e54efb1ed8e172e8852b5d0ef4a57e241f0e
Vasu2803k <katravathvasu000@gmail.com>
2025-09-01 17:35:24 +0000

    Added Autogen AgenticAI-demo files

--- model_definitions/agentic_ai_autogen/model_modules/scoring.py
+++ model_definitions/agentic_ai_autogen/model_modules/scoring.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
+"""
+Scoring module for autogen agent.
+
+This module provides scoring capabilities for the conversational agent.
+"""
+import os
 import json
 from tmo import ModelContext
-import warnings
-warnings.filterwarnings('ignore')
-warnings.simplefilter(action='ignore', category=DeprecationWarning)
-warnings.simplefilter(action='ignore', category=UserWarning)
-warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # Scoring function for evaluating the model performance
 def score(context: ModelContext, **kwargs):
