9e8c8b8031bb03e97df799f4556a7fe1eccddd78
Vasu-Katravath_teradata <vasu.katravath@teradata.com>
2025-09-02 18:21:07 +0530

    Added agentic ai autogen files for testing

--- model_definitions/agentic_ai_autogen/model_modules/training.py
+++ model_definitions/agentic_ai_autogen/model_modules/training.py
@@ -13,4 +13,13 @@ import json
 from tmo import ModelContext
 
 def train(context: ModelContext, **kwargs):
-    print("Training")
+   print("Training...")
+
+   config = {
+         "LLM_MODEL": context.hyperparams["LLM_MODEL"],
+         "LLM_BASE_URL": context.hyperparams["LLM_BASE_URL"],
+         "LLM_API_KEY": context.hyperparams["LLM_API_KEY"],
+      }
+
+   with open(f"{context.artifact_output_path}/model_config.json", "w") as f:
+      json.dump(config, f)
